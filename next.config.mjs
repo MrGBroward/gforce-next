a7b2377e3f2b78199069a9e1826d0cc4c7ab51ff
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-23 14:37:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 120
+  experimental: {
+    appDir: true,
+  },
 };
+
 export default nextConfig;
