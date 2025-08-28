49611480beaa22c959e7d2708833e86fc86dd560
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-28 00:56:30 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: "G-Force Exterior Cleaning",
   description: "Veteran-owned exterior cleaning company specializing in dumpster pad cleaning and professional pressure washing services in Broward, Miami-Dade, and Palm Beach counties.",
   verification: {
-    google-site-verification: google26520857be75da2c.html"
+    google: 26520857be75da2c.html
   }
 };
 
